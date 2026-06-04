ff9244bdc65e751e13cb4528a75da25d3198fdd0
priyanshu raikhere <priyanshuraikhere2002@gmail.com>
2026-06-04 19:30:31 +0530

    Add detailed mongo error logs

--- upgraded/server/config/db.js
+++ upgraded/server/config/db.js
@@ -6,6 +6,8 @@ const connectDB = async () => {
 
     console.log("MongoDB Connected");
   } catch (error) {
+    console.error("MongoDB Connection Error:");
+    console.error(error.message);
     console.error(error);
     process.exit(1);
   }
